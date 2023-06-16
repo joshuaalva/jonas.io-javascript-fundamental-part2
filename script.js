643eb1e3b500e4a02810f342d22f888279b8bea1
Joshua Alvarado <joshalva3@gmail.com>
2023-06-16 05:39:45 -0500

    function review & coding challenge 1 complete arrays next

--- script.js
+++ script.js
@@ -168,15 +168,78 @@
 
 // console.log(fruitProcessor(2, 3));
 
-function percentageOfWorld1(population) {
-  return (population / 7900) * 100;
-}
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
 
-function describePopulation(country, population) {
-  const percentage = percentageOfWorld1(population);
-  const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
-  console.log(description);
-}
+// function describePopulation(country, population) {
+//   const percentage = percentageOfWorld1(population);
+//   const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+//   console.log(description);
+// }
 
-describePopulation("Portugal", 10);
-describePopulation("China", 1441);
+// describePopulation("Portugal", 10);
+// describePopulation("China", 1441);
+
+// Reviewing Functions
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   //   const age = 2037 - birthYear;
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+//   if (retirement > 0) {
+//     return retirement; // return keyword it exits the function immediatley
+//   } else {
+//     return -1;
+//   }
+  //   return `${firstName} retires in ${retirement} years`;
+};
+
+// console.log(yearsUntilRetirement(1993, `Joshua`));
+// console.log(yearsUntilRetirement(1970, `Phil`));
+
+// Coding Challenge # 1
+
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+
+// A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+// Your tasks:
+
+// Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
+
+// Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
+
+// Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
+
+// Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
+
+// Ignore draws this time. Instead, log No team wins... to the console if there is no winner.
+
+// TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
+
+// function checkWinner(scoreDolphins, scoreKoalas) {
+//   const avgDolphins = scoreDolphins;
+//   const avgKoalas = scoreKoalas;
+
+//   if (avgDolphins > avgKoalas) {
+//     console.log(`Dolphins win ${avgDolphins} to ${avgKoalas}`);
+//   }
+
+//   if (avgDolphins > avgKoalas) {
+//     console.log(`Koalas win ${avgKoalas} to ${avgDolphins}`);
+//   } else {
+//     console.log(`No team wins`);
+//   }
+// }
