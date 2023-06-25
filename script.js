3eb6fb160f41a15573af1aff04cdd721af404e7e
Joshua Alvarado <joshalva3@gmail.com>
2023-06-25 09:32:18 -0500

    object methods challenge and review

--- script.js
+++ script.js
@@ -453,37 +453,71 @@
 
 // Object Methods
 
-const jonas = {
-  firstName: "Jonas",
-  lastName: "Schmedtmann",
-  birthYear: 1991,
-  job: "teacher",
-  friends: ["Mike", "Peter", "Steven"],
-  hasDriversLicense: true,
-  // need an expression in an object
-  // just delcaring the function would not word
-  // using calcAge as a 'key'
-  //   calcAge: function (birthYear) {
-  //     return 2037 - birthYear;
-  //   },
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   birthYear: 1991,
+//   job: "teacher",
+//   friends: ["Mike", "Peter", "Steven"],
+//   hasDriversLicense: true,
+// need an expression in an object
+// just delcaring the function would not word
+// using calcAge as a 'key'
+//   calcAge: function (birthYear) {
+//     return 2037 - birthYear;
+//   },
 
-  //   calcAge: function () {
-  //     return 2037 - this.birthYear;
-  //   },
+//   calcAge: function () {
+//     return 2037 - this.birthYear;
+//   },
 
-  calcAge: function () {
-    this.age = 2037 - this.birthYear;
-    return this.age;
-  },
+//   calcAge: function () {
+//     this.age = 2037 - this.birthYear;
+//     return this.age;
+//   },
 
-  getSummary: function () {
-    return `${jonas.firstName} is a ${jonas.calcAge()}-year old ${
-      jonas.job
-    } and he has ${this.hasDriversLicense ? "a" : "no"} driver's license`;
+//   getSummary: function () {
+//     return `${jonas.firstName} is a ${jonas.calcAge()}-year old ${
+//       jonas.job
+//     } and he has ${this.hasDriversLicense ? "a" : "no"} driver's license`;
+//   },
+// };
+
+// console.log(jonas.calcAge());
+// console.log(jonas.getSummary());
+
+// Coding Excercise 7: Challenge #3
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
   },
 };
 
-console.log(jonas.calcAge());
-console.log(jonas.getSummary());
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
 
-// Coding Excercise 7: Challenge #3
+john.calcBMI();
+mark.calcBMI();
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`
+  );
+} else if (john.bmi < mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`
+  );
+}
